e0de3afa3a03add95c6783a598f72b764c3dce8b
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-07-02 13:59:20 +0700

    hero section

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+        center: true,
+        padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#022c22',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
