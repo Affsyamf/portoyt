075b72dc06543a2634ebb7a1c4cdf714c2d4b871
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-07-03 04:15:20 +0700

    navbar section

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+// navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+}
+}
+
+//hum
+const hum = document.querySelector('#hum');
+const navMenu = document.querySelector('#nav-menu');
+
+hum.addEventListener('click', function ()  {
+    hum.classList.toggle('hum-active');
+    navMenu.classList.toggle('hidden');
+});
